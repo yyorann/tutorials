e2571fc5ea58f36b8621b8040cec07b8a16c1998
yyorann <yyorann@yahoo.com>
2023-02-21 14:39:31 +0100

    les 3 - route aangepast

--- laracast-inertia/demo/routes/web.php
+++ laracast-inertia/demo/routes/web.php
@@ -16,6 +16,12 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
+    return Inertia::render('Welcome');
+});
+
+
+
+Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
